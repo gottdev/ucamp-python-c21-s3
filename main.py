59519f736723a8980d0de682d87a8e5457b22026
GottDev <carloswal.jg@gmail.com>
2024-04-18 19:38:26 -0700

    cambios del ejercicio

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("hola mundo")
-print("hola UCamp")
+numero = input("Porfavor ingresa un numero: ")
+numero = int(numero)
+print(numero)
+numero = numero/3
+print(numero)
+"""
+if numero > 2:
+    print("True")
+else:
+    print("False")
+"""
+print(numero > 2)
